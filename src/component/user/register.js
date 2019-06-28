1b9afea4f2df74c48df0273d6493135b50758cc9
Ina <inaeterovic>
2019-06-28 10:15:21 -0400

    saving user withouth authentication

---
+++ src/component/user/register.js
@@ -0,0 +1,65 @@
+import firebase from 'firebase';
+import React from 'react';
+import {db} from '../../firebase';
+
+class Register extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            email:'',
+            password:'',
+            name:''
+        };
+    }
+    handleInputChange = e => {
+        this.setState({
+          [e.target.name]: e.target.value
+        });
+      }
+
+    addUser= e =>{
+        e.preventDefault();
+        const db =firebase.firestore();
+        db.settings({
+            timestampsInSnapshots: true
+          });
+          const userRef = db.collection('users').add({
+            email: this.state.email,
+            password: this.state.password,
+            name: this.state.name
+          });  
+        
+        this.setState({
+            email:'',
+            password:'',
+            name:''
+        })
+    }
+
+    render(){
+    return (
+            <form onSubmit={this.addUser}>
+                <h2>Fill out your information</h2>
+                <input type="text" 
+                       name="email"
+                       placeholder="write your email"
+                       onChange={this.handleInputChange}
+                       value={this.state.email}/>
+                <input type="text" 
+                       name="password"
+                       placeholder="write a password"
+                       onChange={this.handleInputChange}
+                       value={this.state.password}/>
+                <input type="text"
+                       name="name"
+                       placeholder="write your name"
+                       onChange={this.handleInputChange}
+                       value={this.state.name}/>
+                <button type="submit">Register</button>
+            </form>
+        )
+    }
+}
+
+export default Register;
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-
+import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
   
