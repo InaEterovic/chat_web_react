c4239b7b5d602ee14e6fe5143401425f6853d445
Ina <inaeterovic>
2019-07-02 14:47:51 -0400

    refactored sing up, made it usable with hooks

--- src/component/user/signUp.js
+++ src/component/user/signUp.js
@@ -3,62 +3,44 @@ import React,{useCallback } from 'react';
 import {db} from '../../firebase';
 import {withRouter} from 'react-router';
 
-class SignUp extends React.Component{
+const SignUp = ({ history }) => {
 
-    constructor(props){
-        super(props);
-        this.state={
-            email:'',
-            password:'',
-            name:''
-        };
-    }
-    handleInputChange = e => {
-        this.setState({
-          [e.target.name]: e.target.value
-        });
-      }
-
-    addUser= e =>{
-        e.preventDefault();
-        const name = this.state.name;
-        const auth = 
-        firebase.auth().createUserWithEmailAndPassword( this.state.email, this.state.password)
-        .then(()=> {
-          let user = firebase.auth().currentUser;
+  const handleSignUp = useCallback(async event => {
+    event.preventDefault();
+    const { email, password, name } = event.target.elements;
+    try {
+      await firebase.auth()
+      .createUserWithEmailAndPassword( email.value, password.value)
+      .then(()=> {
+              let user = firebase.auth().currentUser;
           user.updateProfile({
-            displayName: name
+            displayName: name.value
           })
-        }).catch(function(error) {
-          console.log(error);
-        });
-        
-        this.props.history.push("/chat");       
+       });       
+      history.push("/chat");
+    } catch (error) {
+      console(error);
     }
+  }, [history]);
 
-    render(){
     return (
-            <form onSubmit={this.addUser}>
+            <form onSubmit={handleSignUp}>
                 <h2>Fill out your information</h2>
                 <input type="email" 
                        name="email"
                        placeholder="Email"
-                       onChange={this.handleInputChange}
-                       value={this.state.email}/>
+                       />
                 <input type="password" 
                        name="password"
                        placeholder="Password"
-                       onChange={this.handleInputChange}
-                       value={this.state.password}/>
+                       />
                 <input type="text"
                        name="name"
                        placeholder="Full Name"
-                       onChange={this.handleInputChange}
-                       value={this.state.name}/>
+                       />
                 <button type="submit">Sign Up</button>
             </form>
-        )
-    }
-}
+        );
+};
 
-export default SignUp;
+export default withRouter(SignUp);
